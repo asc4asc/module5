3ea9f65b650328a106288f5965598567c1ec6db4
Andreas <asc4asc@users.noreply.github.com>
2021-06-08 17:47:02 +0200

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-import * as c1 from './lib/counter.js';
-import { counter } from './lib/counter.js';
-
-console.log("Info1:",counter); // 1
-c1.increment();
-console.log("Info2:",c1.counter); // 2
-c1.decrement();  
-c1.decrement();  
-console.log("Info3:",c1.counter); // 0  
